be98a358575fca123840b451a9403705e0ddaf04
Kasper Jeppe Jeppesen <kasper.jeppesen@gmail.com>
2015-01-28 19:40:20 -0500

    First stab at puzzle mode

---
+++ levels.js
@@ -0,0 +1,72 @@
+var levelData=[
+	{
+		title:"So simple",
+		data:[
+			{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},
+			{n:0,l:false},{n:0,l:false},{n:4,l:false},{n:1,l:false},{n:0,l:false},{n:0,l:false},
+			{n:0,l:false},{n:1,l:false},{n:4,l:false},{n:2,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:1,l:false},{n:1,l:false},{n:3,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:2,l:false},{n:1,l:false},{n:1,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:2,l:false},{n:1,l:false},{n:1,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:3,l:false},{n:3,l:false},{n:4,l:false},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:0,l:false},{n:5,l:true},{n:5,l:false},{n:0,l:false},{n:0,l:false},
+			{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},
+		]
+	},
+	{
+		title:"Might have to think a little",
+		data:[
+			{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},
+			{n:0,l:false},{n:5,l:false},{n:4,l:false},{n:5,l:false},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:5,l:false},{n:3,l:false},{n:2,l:false},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:1,l:false},{n:1,l:false},{n:1,l:false},{n:3,l:false},{n:0,l:false},
+			{n:0,l:false},{n:2,l:false},{n:1,l:false},{n:1,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:2,l:false},{n:1,l:false},{n:1,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:5,l:false},{n:3,l:false},{n:4,l:false},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:0,l:false},{n:6,l:true},{n:5,l:false},{n:0,l:false},{n:0,l:false},
+			{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},
+		]
+	},
+	{
+		title:"What's happening?",
+		data:[
+			{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},
+			{n:0,l:false},{n:5,l:false},{n:4,l:false},{n:6,l:true},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:4,l:false},{n:2,l:false},{n:2,l:false},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:1,l:false},{n:1,l:false},{n:5,l:false},{n:3,l:false},{n:0,l:false},
+			{n:0,l:false},{n:2,l:false},{n:4,l:false},{n:3,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:2,l:false},{n:1,l:false},{n:1,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:5,l:false},{n:3,l:false},{n:4,l:false},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:1,l:false},{n:6,l:true},{n:5,l:false},{n:5,l:false},{n:0,l:false},
+			{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},
+		]
+	},
+	{
+		title:"Who keeps doing this?",
+		data:[
+			{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},
+			{n:0,l:false},{n:3,l:false},{n:4,l:false},{n:5,l:true},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:4,l:false},{n:2,l:false},{n:2,l:false},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:1,l:false},{n:5,l:false},{n:3,l:false},{n:3,l:false},{n:0,l:false},
+			{n:0,l:false},{n:4,l:false},{n:4,l:false},{n:2,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:2,l:false},{n:1,l:false},{n:1,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:5,l:false},{n:3,l:false},{n:4,l:false},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:1,l:false},{n:2,l:true},{n:5,l:false},{n:2,l:false},{n:0,l:false},
+			{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},
+		]
+	},
+	{
+		title:"Probably can't be solved",
+		data:[
+			{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},
+			{n:0,l:false},{n:3,l:false},{n:6,l:false},{n:5,l:true},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:1,l:false},{n:2,l:false},{n:2,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:1,l:false},{n:5,l:false},{n:5,l:false},{n:3,l:false},{n:0,l:false},
+			{n:0,l:false},{n:4,l:false},{n:4,l:false},{n:4,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:2,l:false},{n:1,l:false},{n:1,l:false},{n:1,l:false},{n:0,l:false},
+			{n:0,l:false},{n:5,l:false},{n:3,l:false},{n:4,l:false},{n:4,l:false},{n:0,l:false},
+			{n:0,l:false},{n:1,l:false},{n:2,l:true},{n:5,l:false},{n:2,l:false},{n:0,l:false},
+			{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},{n:0,l:false},
+		]
+	}
+];
